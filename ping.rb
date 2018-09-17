117457a1d7f807700ea4cef23a08f0ebac18198f
Juan Delgado <roloenusa@msn.com>
2018-09-17 00:42:49 -0700

    Add environment variables

--- ping.rb
+++ ping.rb
@@ -1,6 +1,8 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: '', client_id: ''
+puts ENV['CLIENT_SECRET']
+
+bot = Discordrb::Bot.new token: ENV['CLIENT_SECRET'], client_id: ENV['CLIENT_ID']
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
