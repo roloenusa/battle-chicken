09a219657f5c1236e5ff5a64646a7a80eff23a59
Juan Delgado <roloenusa@msn.com>
2018-09-17 00:38:32 -0700

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'discordrb'
+
+ruby '2.4.1'
---
+++ ping.rb
@@ -0,0 +1,9 @@
+require 'discordrb'
+
+bot = Discordrb::Bot.new token: '', client_id: ''
+
+bot.message(with_text: 'Ping!') do |event|
+  event.respond 'Pong!'
+end
+
+bot.run
