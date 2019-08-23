6b857ce7c8381a15b42ab330009aa609e6698f85
Juan Delgado <roloenusa@msn.com>
2019-08-22 23:47:19 -0700

    Listen to messages from warcraft logs.

--- main.rb
+++ main.rb
@@ -15,17 +15,55 @@ report_urls = [
   WIPEFEST,
 ]
 
-bot = Discordrb::Commands::CommandBot.new token: client_secret, prefix: '!'
+# bot = Discordrb::Commands::CommandBot.new token: client_secret, prefix: '!'
 
-bot.command :report do |event, *args|
-  # Load the event with what we're sending to the channel.
-  report = args.first
-  report_urls.each do |url|
-    event << "#{url}#{report}"
+# bot.command :report do |event, *args|
+#   # Load the event with what we're sending to the channel.
+#   report = args.first
+#   report_urls.each do |url|
+#     event << "#{url}#{report}"
+#   end
+
+#   # Return nil
+#   return
+# end
+
+# bot.run
+
+botCapture = Discordrb::Bot.new token: client_secret
+
+botCapture.message() do |event|
+  print "#{event.message.to_s}\n"
+  # print "#{event.username}\n"
+  username = event.user.username;
+  message = event.message;
+
+  if username == "Warcraft Logs" or username == 'Sodastereo' then
+    parts = message.to_s.split("\n")
+    urlParts =  parts[1].split("/");
+
+    # Load the event with what we're sending to the channel.
+    i = -1;
+    urlParts.each_with_index do |part, index|
+      if part == 'reports' then
+        i = index;
+        break;
+      end
+    end
+
+    # The response we'll send to the server.
+    result = [];
+    result << "\n**[#{parts.last}]**\n\n"
+
+    # Get the actual report.
+    report = urlParts[i + 1]
+    report_urls.each do |url|
+      result << "#{url}#{report}"
+    end
+
+    # Send the response back to the server.
+    event.respond result.join("\n")
   end
-
-  # Return nil
-  return
 end
 
-bot.run
+botCapture.run
